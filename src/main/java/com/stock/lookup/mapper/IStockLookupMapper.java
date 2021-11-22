6294141115bc7a716436e3c88d3b0228f694475f
SarathChandraBellam <sarathbellam95@gmail.com>
2021-11-23 02:08:14 +0530

    adopting mapstruct for mappers

---
+++ src/main/java/com/stock/lookup/mapper/IStockLookupMapper.java
@@ -0,0 +1,19 @@
+package com.stock.lookup.mapper;
+
+import com.stock.lookup.dto.StockLookUpDTO;
+import com.stock.lookup.model.StockLookUp;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+@Mapper(componentModel = "spring")
+public interface IStockLookupMapper {
+    @Mapping(source = "stockName",target = "stockName",qualifiedByName = "stockUpperCase")
+    StockLookUp fromDto(StockLookUpDTO stockLookUpDTO);
+    StockLookUpDTO toDto(StockLookUp stockLookUp);
+
+    @Named("stockUpperCase")
+    default String toUpperCaseStockName(String name){
+        return name.toUpperCase();
+    }
+}
--- src/main/java/com/stock/lookup/service/StockLookupService.java
+++ src/main/java/com/stock/lookup/service/StockLookupService.java
@@ -2,7 +2,7 @@ package com.stock.lookup.service;
 
 import com.stock.lookup.dao.LookUpRepository;
 import com.stock.lookup.dto.StockLookUpDTO;
-import com.stock.lookup.mapper.StockLookupMapper;
+import com.stock.lookup.mapper.IStockLookupMapper;
 import com.stock.lookup.model.StockLookUp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,97 +17,98 @@ import java.util.stream.Collectors;
 @Service
 public class StockLookupService {
 
-  private final LookUpRepository lookUpRepository;
-  @Autowired StockLookupMapper stockLookupMapper;
+    private final LookUpRepository lookUpRepository;
+    @Autowired
+    IStockLookupMapper stockLookupMapper;
 
-  public StockLookupService(LookUpRepository lookUpRepository) {
-    this.lookUpRepository = lookUpRepository;
-  }
+    public StockLookupService(LookUpRepository lookUpRepository) {
+        this.lookUpRepository = lookUpRepository;
+    }
 
-  // Retrieve
-  // list
-  public List<StockLookUpDTO> getAllSuggestionsBasedONGroupName(String groupName) {
-    List<StockLookUp> stockLookUps = lookUpRepository.findByGroupName(groupName);
-    return stockLookUps.parallelStream().map(stockLookupMapper::toDto).collect(Collectors.toList());
-  }
+    // Retrieve
+    // list
+    public List<StockLookUpDTO> getAllSuggestionsBasedONGroupName(String groupName) {
+        List<StockLookUp> stockLookUps = lookUpRepository.findByGroupName(groupName);
+        return stockLookUps.parallelStream().map(stockLookupMapper::toDto).collect(Collectors.toList());
+    }
 
-  // single:
-  public StockLookUpDTO getSuggestionBasedOnGroupandStockName(String groupName, String stockName) {
-    StockLookUp stockLookUp =
-        lookUpRepository
-            .findByGroupNameAndStockName(groupName, stockName)
-            .orElseThrow(EntityNotFoundException::new);
-    return stockLookupMapper.toDto(stockLookUp);
-  }
+    // single:
+    public StockLookUpDTO getSuggestionBasedOnGroupandStockName(String groupName, String stockName) {
+        StockLookUp stockLookUp =
+                lookUpRepository
+                        .findByGroupNameAndStockName(groupName, stockName)
+                        .orElseThrow(EntityNotFoundException::new);
+        return stockLookupMapper.toDto(stockLookUp);
+    }
 
-  // singleLike:
-  public List<StockLookUpDTO> getSuggestionBasedOnGroupandStockNameLike(
-      String groupName, String stockName) {
-    List<StockLookUp> stockLookUpList =
-        lookUpRepository.findByGroupNameAndStockNameContaining(groupName, stockName);
-    return stockLookUpList
-        .parallelStream()
-        .map(stockLookupMapper::toDto)
-        .collect(Collectors.toList());
-  }
+    // singleLike:
+    public List<StockLookUpDTO> getSuggestionBasedOnGroupandStockNameLike(
+            String groupName, String stockName) {
+        List<StockLookUp> stockLookUpList =
+                lookUpRepository.findByGroupNameAndStockNameContaining(groupName, stockName);
+        return stockLookUpList
+                .parallelStream()
+                .map(stockLookupMapper::toDto)
+                .collect(Collectors.toList());
+    }
 
-  // create
-  public StockLookUpDTO createNewSuggestion(StockLookUpDTO stockLookUpDTO) {
-    StockLookUp stockLookUp = stockLookupMapper.fromDto(stockLookUpDTO);
-    StockLookUp saveState = lookUpRepository.save(stockLookUp);
-    return stockLookupMapper.toDto(saveState);
-  }
+    // create
+    public StockLookUpDTO createNewSuggestion(StockLookUpDTO stockLookUpDTO) {
+        StockLookUp stockLookUp = stockLookupMapper.fromDto(stockLookUpDTO);
+        StockLookUp saveState = lookUpRepository.save(stockLookUp);
+        return stockLookupMapper.toDto(saveState);
+    }
 
-  public List<StockLookUpDTO> createNewSuggestions(List<StockLookUpDTO> stockLookUpDTOs) {
-    List<StockLookUp> stockLookUps =
-        stockLookUpDTOs
-            .parallelStream()
-            .map(stockLookupMapper::fromDto)
-            .collect(Collectors.toList());
-    return lookUpRepository
-        .saveAll(stockLookUps)
-        .parallelStream()
-        .map(stockLookupMapper::toDto)
-        .collect(Collectors.toList());
-  }
+    public List<StockLookUpDTO> createNewSuggestions(List<StockLookUpDTO> stockLookUpDTOs) {
+        List<StockLookUp> stockLookUps =
+                stockLookUpDTOs
+                        .parallelStream()
+                        .map(stockLookupMapper::fromDto)
+                        .collect(Collectors.toList());
+        return lookUpRepository
+                .saveAll(stockLookUps)
+                .parallelStream()
+                .map(stockLookupMapper::toDto)
+                .collect(Collectors.toList());
+    }
 
-  // Update::
-  public StockLookUpDTO updateOldSuggestionOrCreateNew(
-      @RequestBody StockLookUpDTO newStockLookUpDTO,
-      @PathVariable String groupName,
-      @PathVariable String stockName) {
-    return lookUpRepository
-        .findByGroupNameAndStockName(groupName, stockName)
-        .map(
-            lookUp -> {
-              lookUp.setBuyStartRange(newStockLookUpDTO.buyStartRange);
-              lookUp.setBuyEndRange(newStockLookUpDTO.buyEndRange);
-              lookUp.setWeightAge(newStockLookUpDTO.weightAge);
-              StockLookUp stockLookUp = lookUpRepository.save(lookUp);
-              return stockLookupMapper.toDto(stockLookUp);
-            })
-        .orElseGet(
-            () -> {
-              StockLookUp stockLookUp = stockLookupMapper.fromDto(newStockLookUpDTO);
-              StockLookUp updateSaveState = lookUpRepository.save(stockLookUp);
-              return stockLookupMapper.toDto(updateSaveState);
-            });
-  }
+    // Update::
+    public StockLookUpDTO updateOldSuggestionOrCreateNew(
+            @RequestBody StockLookUpDTO newStockLookUpDTO,
+            @PathVariable String groupName,
+            @PathVariable String stockName) {
+        return lookUpRepository
+                .findByGroupNameAndStockName(groupName, stockName)
+                .map(
+                        lookUp -> {
+                            lookUp.setBuyStartRange(newStockLookUpDTO.buyStartRange);
+                            lookUp.setBuyEndRange(newStockLookUpDTO.buyEndRange);
+                            lookUp.setWeightAge(newStockLookUpDTO.weightAge);
+                            StockLookUp stockLookUp = lookUpRepository.save(lookUp);
+                            return stockLookupMapper.toDto(stockLookUp);
+                        })
+                .orElseGet(
+                        () -> {
+                            StockLookUp stockLookUp = stockLookupMapper.fromDto(newStockLookUpDTO);
+                            StockLookUp updateSaveState = lookUpRepository.save(stockLookUp);
+                            return stockLookupMapper.toDto(updateSaveState);
+                        });
+    }
 
-  // deleteByGroupName
-  // list
-  public void deleteAllSuggestionsBasedONGroupName(String groupName) {
-    List<StockLookUp> stockLookUps = lookUpRepository.findByGroupName(groupName);
-    lookUpRepository.deleteAll(stockLookUps);
-  }
+    // deleteByGroupName
+    // list
+    public void deleteAllSuggestionsBasedONGroupName(String groupName) {
+        List<StockLookUp> stockLookUps = lookUpRepository.findByGroupName(groupName);
+        lookUpRepository.deleteAll(stockLookUps);
+    }
 
-  // single
-  @Transactional
-  public void deleteSuggestionBasedOnGroupandStockName(String groupName, String stockName) {
-    StockLookUp stockLookUp =
-        lookUpRepository
-            .findByGroupNameAndStockName(groupName, stockName)
-            .orElseThrow(EntityNotFoundException::new);
-    lookUpRepository.deleteByGroupNameAndStockName(groupName, stockName);
-  }
+    // single
+    @Transactional
+    public void deleteSuggestionBasedOnGroupandStockName(String groupName, String stockName) {
+        StockLookUp stockLookUp =
+                lookUpRepository
+                        .findByGroupNameAndStockName(groupName, stockName)
+                        .orElseThrow(EntityNotFoundException::new);
+        lookUpRepository.deleteByGroupNameAndStockName(groupName, stockName);
+    }
 }
